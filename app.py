d31ee885ee5cc76a564488023bcc30bc4196bb09
rasvanjaya21 <mrsejati17@gmail.com>
2023-03-25 02:05:59 +0700

    beautifull billy

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ code = '''def predict():
    """
    print("your learning style is {predict_result}")'''
 
-st.code(code, language='python')
+st.code(code, language="python")
 st.caption("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis risus dui. Fusce a finibus libero. Vivamus consequat elit orci, non gravida enim vehicula vel.", unsafe_allow_html=False)
 
 st.subheader("# 01 - multiple choices answer")
@@ -37,35 +37,40 @@ with st.form("choices_form"):
 
    choices_checkbox = st.checkbox("Validation Test")
 
-   # Every form must have a submit button.
    choices_submitted = st.form_submit_button("Predict Multiple Choices Learning Style", use_container_width=True)
    
    if choices_submitted:
-	   st.session_state['multiple_choices_answer_state'] = str(choices_checkbox)
+	   st.session_state["multiple_choices_answer_state"] = str(choices_checkbox)
 
-if 'multiple_choices_answer_state' not in st.session_state:
+if "multiple_choices_answer_state" not in st.session_state:
    st.warning("Please fill parameters above to see the prediction result !")
 
 else:
-   st.success("Your Multiple Choices Answer Learning Style Is " + str(choices_checkbox) )
+   if choices_checkbox:
+      st.success("Your Multiple Choices Answer Learning Style Is " + str(choices_checkbox))
+   else:
+      st.error("Your Multiple Choices Answer Learning Style Is " + str(choices_checkbox) )
 
 st.subheader("# 02 - essay answer")
 
 with st.form("essays_form"):
+   
    st.code("COMING SOON")
    essays_checkbox = st.checkbox("Validation Test")
    
-   # Every form must have a submit button.
    essays_submitted = st.form_submit_button("Predict Learning Style", use_container_width=True)
    
    if essays_submitted:
-	   st.session_state['esssay_answer_state'] = str(essays_checkbox)
+	   st.session_state["esssay_answer_state"] = str(essays_checkbox)
 
-if 'esssay_answer_state' not in st.session_state:
+if "esssay_answer_state" not in st.session_state:
    st.warning("Please fill parameters above to see the prediction result !")
 
 else:
-   st.success("Your Essays Answer Learning Style Is " + str(essays_checkbox) )
+   if essays_checkbox:
+      st.success("Your Essays Answer Learning Style Is " + str(essays_checkbox))
+   else:
+      st.error("Your Essays Answer Learning Style Is " + str(essays_checkbox))
 
 configurations = """
    <style>
